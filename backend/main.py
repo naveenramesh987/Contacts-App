414ad5d7c2370658584b3ed0cc20a10a20eed25d
Naveen Ramesh <naveenramesh987@gmail.com>
2024-03-26 10:36:43 -0700

    Delete contacts button

--- backend/main.py
+++ backend/main.py
@@ -22,6 +22,42 @@ def create_contact():
             400,
         )
 
+    new_contact = Contact(first_name=first_name, last_name=last_name, email=email)
+    try:
+        db.session.add(new_contact)
+        db.session.commit()
+    except Exception as e:
+        return jsonify({"message": str(e)}), 400
+
+    return jsonify({"message": "User created!"}), 201
+
+
+@app.route("/update_contact/<int:user_id>")
+def update_contact(user_id):
+    contact = Contact.query.get(user_id)
+    if not contact:
+        return jsonify({"message": "User not found"}), 404
+
+    data = request.json
+    contact.first_name = data.get("firstName", contact.first_name)
+    contact.last_name = data.get("lastName", contact.last_name)
+    contact.email = data.get("email", contact.email)
+
+    db.session.commit()
+    return jsonify({"message": "User updated."}), 200
+
+
+@app.route("/delete_contact/<int:user_id>", methods=["DELETE"])
+def delete_contact(user_id):
+    contact = Contact.query.get(user_id)
+
+    if not contact:
+        return jsonify({"message": "User not found"}), 404
+
+    db.session.delete(contact)
+    db.session.commit()
+    return jsonify({"message": "User deleted!"}), 200
+
 
 if __name__ == "__main__":
     with app.app_context():
